26da4c32c9934bacb7ff700ed5064b8b33425fbd
panhezeng <panhezeng@gmail.com>
2023-05-19 11:02:20 +0800

    feat: 调整ui

--- packages/main/dist/index.cjs
+++ packages/main/dist/index.cjs
@@ -19,7 +19,7 @@ async function createWindow() {
       browserWindow?.webContents.openDevTools();
     }
   });
-  const pageUrl = "http://localhost:5173/";
+  const pageUrl = "http://127.0.0.1:5173/";
   await browserWindow.loadURL(pageUrl);
   return browserWindow;
 }
