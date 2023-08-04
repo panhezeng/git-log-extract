e7ea493d5bc599c81829de825ee8cae4b5c47336
panhezeng <panhezeng@gmail.com>
2023-08-04 18:19:45 +0800

    feat:替换lint-staged

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   'packages/**/src/**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts,vue}': 'eslint --cache --fix',
   'packages/**/src/**/*.{ts,tsx,cts,mts,vue}': 'npm run type-check',
 };
